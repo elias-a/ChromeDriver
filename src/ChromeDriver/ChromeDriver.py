e42886621c65f01fb93793e5f4fb0cf1b687885e
elias-a <elias.alstead@gmail.com>
2023-02-19 18:37:50 -0500

    Pass config variables into class constructor instead of passing the path to config file

--- src/ChromeDriver/ChromeDriver.py
+++ src/ChromeDriver/ChromeDriver.py
@@ -3,50 +3,46 @@ import os
 import signal
 import pathlib
 import subprocess
-from configparser import ConfigParser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 class ChromeDriver:
-    def __init__(self, configPath):
-        self.config = ConfigParser()
-        self.config.read(configPath)
-
-        self.initDriver()
+    def __init__(self, chromedriver, port, chrome, profile):
+        self._chromedriver = chromedriver
+        self._port = port
+        self._chrome = chrome
+        self._profile = profile
+        self.driver = None
+        self._process = None
 
     @staticmethod
-    def isPortOpen(port):
+    def _isPortOpen(port):
         completedProcess = subprocess.run(["lsof", f"-i:{port}"], 
             stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         return completedProcess.returncode != 0
 
     def initDriver(self):
-        chromedriver = self.config["CHROME"]["chromedriverPath"]
-        self.chromePort = int(self.config["CHROME"]["chromePort"])
-
         options = Options()
         options.headless = True
-        options.add_experimental_option("debuggerAddress", f"127.0.0.1:{self.chromePort}")
+        options.add_experimental_option("debuggerAddress", f"127.0.0.1:{self._port}")
 
-        if ChromeDriver.isPortOpen(self.chromePort):
-            self.openChrome()
+        if ChromeDriver._isPortOpen(self._port):
+            self._openChrome()
         else:
-            print(f"Port {self.chromePort} is taken")
+            print(f"Port {self._port} is taken")
             sys.exit(1)
 
-        self.driver = webdriver.Chrome(chromedriver, options=options)
+        self.driver = webdriver.Chrome(self._chromedriver, options=options)
 
-    def openChrome(self):
-        chromePath = self.config["CHROME"]["chromePath"]
-        chromeUserDataDir = self.config["CHROME"]["chromeUserDataDir"]
-        self.chromeProcess = subprocess.Popen([
+    def _openChrome(self):
+        self._process = subprocess.Popen([
             f"{pathlib.Path(__file__).parent.resolve()}/start_chrome.sh",
-            chromePath,
-            str(self.chromePort),
-            chromeUserDataDir
+            self._chrome,
+            str(self._port),
+            self._profile,
         ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, preexec_fn=os.setsid)
 
     def closeChrome(self):
-        os.killpg(os.getpgid(self.chromeProcess.pid), signal.SIGTERM)
+        os.killpg(os.getpgid(self._process.pid), signal.SIGTERM)
         self.driver.quit()
 
