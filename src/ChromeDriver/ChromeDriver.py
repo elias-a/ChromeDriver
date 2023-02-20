5fbeceaf66d10a67a880d29fe5ce05d6c08ece5c
elias-a <elias.alstead@gmail.com>
2023-02-20 12:32:37 -0500

    Import chromedriver binary and launch Chrome as a new process

--- src/ChromeDriver/ChromeDriver.py
+++ src/ChromeDriver/ChromeDriver.py
@@ -3,14 +3,14 @@ import os
 import signal
 import pathlib
 import subprocess
+import chromedriver_binary
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 class ChromeDriver:
-    def __init__(self, chromedriver, port, chrome, profile):
-        self._chromedriver = chromedriver
+    def __init__(self, path, port, profile):
+        self._path = path
         self._port = port
-        self._chrome = chrome
         self._profile = profile
         self.driver = None
         self._process = None
@@ -22,24 +22,21 @@ class ChromeDriver:
         return completedProcess.returncode != 0
 
     def initDriver(self):
-        options = Options()
-        options.headless = True
-        options.add_experimental_option("debuggerAddress", f"127.0.0.1:{self._port}")
-
         if ChromeDriver._isPortOpen(self._port):
             self._openChrome()
         else:
-            print(f"Port {self._port} is taken")
-            sys.exit(1)
+            raise Exception(f"Port {self._port} is taken")
 
-        self.driver = webdriver.Chrome(self._chromedriver, options=options)
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument(f"--remote-debugging-port={self._port}")
+        self.driver = webdriver.Chrome(options=options)
 
     def _openChrome(self):
         self._process = subprocess.Popen([
-            f"{pathlib.Path(__file__).parent.resolve()}/start_chrome.sh",
-            self._chrome,
-            str(self._port),
-            self._profile,
+            self._path,
+            f"--remote-debugging-port={self._port}",
+            f"--user-data-dir={self._profile}",
         ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, preexec_fn=os.setsid)
 
     def closeChrome(self):
