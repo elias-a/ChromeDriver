42a4ce311f2d2fe1cc61cfdded59acaa73add8ac
elias-a <elias.alstead@gmail.com>
2024-11-11 09:24:08 -0500

    Make timeout an instance attribute

--- src/ChromeDriver/ChromeDriver.py
+++ src/ChromeDriver/ChromeDriver.py
@@ -5,7 +5,8 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 class ChromeDriver:
-    def __init__(self, user_data_dir, headless=False):
+    def __init__(self, user_data_dir, headless=False, timeout=10):
+        self.timeout = timeout
         self.driver = self._init_driver(user_data_dir, headless)
 
     def __del__(self):
@@ -19,12 +20,15 @@ class ChromeDriver:
         [options.add_experimental_option(k, v) for k, v in opts.items()]
         return options
 
-    def send_keys(self, xpath, text, timeout=10):
+    def get(self, url):
+        self.driver.get(url)
+
+    def send_keys(self, xpath, text):
         is_loaded = EC.presence_of_element_located((By.XPATH, xpath))
-        element = WebDriverWait(self.driver, timeout).until(is_loaded)
+        element = WebDriverWait(self.driver, self.timeout).until(is_loaded)
         element.send_keys(text)
 
-    def click(self, xpath, timeout=10):
+    def click(self, xpath):
         is_loaded = EC.element_to_be_clickable((By.XPATH, xpath))
-        element = WebDriverWait(self.driver, timeout).until(is_loaded)
+        element = WebDriverWait(self.driver, self.timeout).until(is_loaded)
         self.driver.execute_script("arguments[0].click();", element)
